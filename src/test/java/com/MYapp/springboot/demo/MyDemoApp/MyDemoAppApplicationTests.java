d908751df8012e908fe24bbf8420c406d74e0ff7
Pawan Nishad <anshpn8@gmail.com>
2024-01-27 15:15:07 +0530

    Initial commit of created project

---
+++ src/test/java/com/MYapp/springboot/demo/MyDemoApp/MyDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MYapp.springboot.demo.MyDemoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
